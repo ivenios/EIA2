ec934d40410fbc8d2a0f484f9a6161feaf44d37f
Iven Otis Sieglen <iven.otis.sieglen@gmail.com>
2019-05-02 16:41:14 +0200

    servertest

---
+++ Aufgabe_6/EisDealerNode/TS/data.ts
@@ -0,0 +1,129 @@
+
+namespace EisDealerFreude {
+    /*
+        Aufgabe: Aufgabe 5 Eisdealer Reloaded
+        Name: Iven Otis Sieglen 
+        Matrikel: 261012
+        Datum: Sonntag der 28. April
+            
+        Hiermit versichere ich, dass ich diesen Code selbst geschrieben habe. Er wurde nicht kopiert und auch nicht diktiert.
+        */
+
+    export let iceDealerData: {[group: string]: fieldsetData[]}
+		= {
+		"Eissorten": [
+            //zuerst die Optgroups, value wird dabei als ID verwendet,wo die einzelnen Eissorten hin müssen
+			//{name:"Langweilige Eissorten", type:"optgroup", value:"IceOptionsOne" , price: 0, inStock: true },
+            //{name:"Moderne Eissorten", type:"optgroup", value:"IceOptionsTwo" , price: 0, inStock: true },
+            //dann werden die Eissorten sortiert 
+            {name:"Erdbeere", type:"options", value:"Langweilige Eissorten" , price: 1.55, inStock: true },
+            {name:"Vanille", type:"options", value:"Langweilige Eissorten" , price: 1.55, inStock: true },
+            {name:"Schokolade", type:"options", value:"Langweilige Eissorten" , price: 1.55, inStock: true },
+            {name:"Mango", type:"options", value:"Langweilige Eissorten" , price: 1.55, inStock: true },
+            {name:"Pistanzie", type:"options", value:"Langweilige Eissorten" , price: 1.55, inStock: true },
+            {name:"Walnuss", type:"options", value:"Langweilige Eissorten" , price: 1.55, inStock: true },
+            {name:"Haselnuss", type:"options", value:"Langweilige Eissorten" , price: 1.55, inStock: true },
+            {name:"Dunkle Schokolade", type:"options", value:"Langweilige Eissorten" , price: 1.55, inStock: true },
+            {name:"Jogurt", type:"options", value:"Langweilige Eissorten" , price: 1.55, inStock: true },
+            {name:"Zitrone", type:"options", value:"Langweilige Eissorten" , price: 1.55, inStock: true },
+            {name:"Melone", type:"options", value:"Langweilige Eissorten" , price: 1.55, inStock: true },
+            {name:"Cassis", type:"options", value:"Langweilige Eissorten" , price: 1.55, inStock: true },
+            {name:"Blaubeere", type:"options", value:"Langweilige Eissorten" , price: 1.55, inStock: true },
+            //optgroup2
+            {name:"Karamell-Meersalz", type:"options", value:"Moderne Eissorten" , price: 1.55, inStock: true },
+            {name:"Weiße Schokolade", type:"options", value:"Moderne Eissorten" , price: 1.55, inStock: true },
+            {name:"Hard Time ;)", type:"options", value:"Moderne Eissorten" , price: 1.55, inStock: true },
+            {name:"Mutterlich", type:"options", value:"Moderne Eissorten" , price: 1.55, inStock: true },
+            {name:"Pferdefleisch", type:"options", value:"Moderne Eissorten" , price: 1.55, inStock: true },
+            {name:"Tintenfischtinte", type:"options", value:"Moderne Eissorten" , price: 1.55, inStock: true },
+            {name:"Braunkohle", type:"options", value:"Moderne Eissorten" , price: 1.55, inStock: true },
+            {name:"Farbwechselnde Eiscreme", type:"options", value:"Moderne Eissorten" , price: 1.55, inStock: true },
+            {name:"Krokodileier", type:"options", value:"Moderne Eissorten" , price: 1.55, inStock: true },
+            {name:"Tierfette", type:"options", value:"Moderne Eissorten" , price: 1.55, inStock: true },
+            {name:"Hummer", type:"options", value:"Moderne Eissorten" , price: 1.55, inStock: true },
+            {name:"Cheesburger", type:"options", value:"Moderne Eissorten" , price: 1.55, inStock: true },
+            {name:"Kiwi", type:"options", value:"Moderne Eissorten" , price: 1.55, inStock: true },
+            {name:"Beton&Asphalt", type:"options", value:"Moderne Eissorten" , price: 1.55, inStock: true },
+            {name:"Euroklassen 1-5", type:"options", value:"Moderne Eissorten" , price: 1.55, inStock: true },
+            {name:"Black Mamba", type:"options", value:"Moderne Eissorten" , price: 1.55, inStock: true },
+            {name:"Weißbier-Eis", type:"options", value:"Moderne Eissorten" , price: 1.55, inStock: true },
+            {name:"Erdbeer-Balsamico", type:"options", value:"Moderne Eissorten" , price: 1.55, inStock: true },
+            {name:"Matcha-Eis", type:"options", value:"Moderne Eissorten" , price: 1.55, inStock: true },
+            {name:"Acai-Beeren", type:"options", value:"Moderne Eissorten" , price: 1.55, inStock: true },
+            {name:"Rote-Bete", type:"options", value:"Moderne Eissorten" , price: 1.55, inStock: true },
+            {name:"Sellerie-Eis", type:"options", value:"Moderne Eissorten" , price: 1.55, inStock: true }
+		],
+		"Streußel": [
+            {name:"Schoko-Steußel", type:"checkbox", value:"Schoko-Steußel" , price: 0.80, inStock: true},
+            {name:"Snickers", type:"checkbox", value:"Snickers" , price: 0.80, inStock: true},
+            {name:"Tropical", type:"checkbox", value:"Tropical" , price: 0.80, inStock: true},
+            {name:"Schokolinsen", type:"checkbox", value:"Schokolinsen" , price: 0.80, inStock: true},
+            {name:"Lions-Steußel", type:"checkbox", value:"Lions-Steußel" , price: 0.80, inStock: true},
+            {name:"Bounty-Steußel", type:"checkbox", value:"Bounty-Steußel" , price: 0.80, inStock: true},
+            {name:"Kit-Kat-Steußel", type:"checkbox", value:"Kit-Kat-Steußel" , price: 0.80, inStock: true},
+            {name:"Buchstabenstreußel", type:"checkbox", value:"Buchstabenstreußel" , price: 0.80, inStock: true},
+            {name:"HFU-Steußel", type:"checkbox", value:"HFU-Steußel" , price: 0.80, inStock: true}
+        ],
+        "Saucen": [
+            {name:"Schoko-Sauce", type:"checkbox", value:"Schoko-Sauce" , price: 0.8, inStock: true},
+            {name:"Karamell-Sauce", type:"checkbox", value:"Karamell-Sauce" , price: 0.8, inStock: true},
+            {name:"Vanille-Sauce", type:"checkbox", value:"Vanille-Sauce" , price: 0.8, inStock: true},
+            {name:"Himbeere-Sauce", type:"checkbox", value:"Himbeere-Sauce" , price: 0.8, inStock: true},
+            {name:"Schlumpf-Sauce", type:"checkbox", value:"" , price: 0.8, inStock: true},
+            {name:"Apfel-Sauce", type:"checkbox", value:"" , price: 0.8, inStock: true},
+            {name:"Erdbeere-Sauce", type:"checkbox", value:"" , price: 0.8, inStock: true},
+            {name:"Salzsäure-Sauce", type:"checkbox", value:"" , price: 0.8, inStock: true},
+            {name:"Schaaf-Sauce", type:"checkbox", value:"" , price: 0.8, inStock: false},
+            {name:"Döner-Sauce", type:"checkbox", value:"" , price: 0.8, inStock: false},
+            {name:"-Sauce", type:"checkbox", value:"" , price: 0.8, inStock: false},
+            
+        ],
+        "Specials":[
+            {name:"Wasabi", type:"checkbox", value:"" , price: 0.8, inStock: true},
+            {name:"Geschmeltze Zwiebeln", type:"checkbox", value:"" , price: 0.8, inStock: true},
+            {name:"Ibuprofen 800mg", type:"checkbox", value:"" , price: 0.8, inStock: true},
+            {name:"Tabasco", type:"checkbox", value:"" , price: 0.8, inStock: true},
+            {name:"Parmesan", type:"checkbox", value:"" , price: 0.8, inStock: true},
+        ],
+        "Sahne":[
+            {name:"Gewöhnliche Schlagsahne", type:"checkbox", value:"" , price: 0.8, inStock: true},
+            {name:"Lactose-Frei", type:"checkbox", value:"" , price: 0.8, inStock: true},
+            {name:"Vegane Sahneersatzprodukte", type:"checkbox", value:"" , price: 0.8, inStock: true},
+            {name:"Schokosahne", type:"checkbox", value:"" , price: 0.8, inStock: true},
+            {name:"Mehr Sahne als Eis", type:"checkbox", value:"" , price: 0.8, inStock: true},
+        ],
+        "Becher":[
+            {name:"Im Becher (kostenlos)", type:"radio", value:"radiogroup1" , price: 0, inStock: true},
+            {name:"In der Waffel (kostenlos)", type:"radio", value:"radiogroup1" , price: 0, inStock: true},
+            {name:"In der Schoko-Waffel (kostenlos)", type:"radio", value:"radiogroup1" , price: 0, inStock: true},
+        ],
+        "Löffel":[
+            {name:"Löffel", type:"radio", value:"radiogroup2" , price: 0, inStock: true},
+            {name:"Gabel", type:"radio", value:"radiogroup2" , price: 0, inStock: true},
+            {name:"Stäbchen", type:"radio", value:"radiogroup2" , price: 0, inStock: true},
+            {name:"Wir sind zu Zweit", type:"radio", value:"radiogroup2" , price: 0, inStock: true},
+        ],
+        "Lieferung":[
+            {name:"Same Day", type:"radio", value:"radiogroup3" , price: 0, inStock: true},
+            {name:"Same Hour", type:"radio", value:"radiogroup3" , price: 0, inStock: true},
+            {name:"5-10 Werktage", type:"radio", value:"radiogroup3" , price: 0, inStock: true},
+            {name:"Beamen", type:"radio", value:"radiogroup3" , price: 0, inStock: true},
+        ],
+        "Bestellübersicht":[
+            {name:"", type:"", value:"" , price: 0, inStock: true},
+        ]
+
+	};
+
+
+
+
+
+
+
+
+
+
+
+
+}
---
+++ Aufgabe_6/EisDealerNode/TS/main.ts
@@ -0,0 +1,413 @@
+
+namespace EisDealerFreude {
+    /*
+        Aufgabe: Aufgabe 6 Eisdealer
+        Name: Iven Otis Sieglen 
+        Matrikel: 261012
+        Datum: Sonntag der 1. Mai
+            
+        Hiermit versichere ich, dass ich diesen Code selbst geschrieben habe. Er wurde nicht kopiert und auch nicht diktiert.
+        */
+
+
+    document.addEventListener('DOMContentLoaded', init);
+    let lableNum: number = 0;
+    export interface fieldsetData {
+        name: string;
+        type: string;
+        value: string;
+        price: number;
+        inStock: boolean;
+    }
+
+    function init():void {
+        renderFieldsets();
+        addListeners();
+
+    }
+
+    function renderFieldsets(): void {
+        
+
+        for (let group in iceDealerData) {
+            if (group == "Eissorten") {//mit forschleife und iceDealerData[group][hier mit for schleife durch gehen] und dann die einzelnen types mit if abfrage abfragen 
+                console.log("Eissorten sind da");
+                for (let i: number = 0; i < iceDealerData[group].length; i++) {
+                    if (iceDealerData[group][i].type == "options") {
+                        console.log("Eissorte " + iceDealerData[group][i].name + " wird geladen")
+                        if (iceDealerData[group][i].inStock == true) {
+                            if (iceDealerData[group][i].value == "Langweilige Eissorten") {
+                                let a: number = 1;
+                                while (a <= 4) {
+                                    writeHTMLIceFlavor(iceDealerData[group][i], "IceOptionsOne" + a);
+                                    a++;
+                                }
+
+                            }
+                            else if (iceDealerData[group][i].value == "Moderne Eissorten") {
+                                let a: number = 1;
+                                while (a <= 4) {
+                                    writeHTMLIceFlavor(iceDealerData[group][i], "IceOptionsTwo" + a);
+                                    a++;
+                                }
+
+
+                            }
+                        }
+                    }
+
+                }
+
+                //hier eine funktion die sich um die Eissorten kümmert
+            }
+            else if (group == "Streußel" || group == "Saucen" || group == "Specials" || group == "Sahne") {
+                //hier dann eine funtkion die sich allein um die Checkboxen kümmert
+                console.log("Toppings werden geladen");
+
+                for (let i: number = 0; i < iceDealerData[group].length; i++) {
+                    if (iceDealerData[group][i].inStock == true) {
+                        writeHTML(iceDealerData[group][i], group, "Lable" + lableNum);
+                        lableNum++; //diese Variable ist dafür da, dass jeder Input und das dazugehörige Label eine gleiche ID bekommen, damit sie zusammen passen. 
+                    }
+                }
+            }
+            else if (group == "Becher" || group == "Löffel" || group == "Lieferung") {
+                for (let i: number = 0; i < iceDealerData[group].length; i++) {
+                    if (iceDealerData[group][i].inStock == true) {
+                        writeHTML(iceDealerData[group][i], group, "Lable" + lableNum);
+                        lableNum++; //diese Variable ist dafür da, dass jeder Input und das dazugehörige Label eine gleiche ID bekommen, damit sie zusammen passen. 
+                    }
+                }
+            }
+            
+        }
+    }
+
+    function addListeners(): void {
+        let formSet: HTMLCollectionOf<HTMLFieldSetElement> = document.getElementsByTagName('fieldset'); //alle fieldsets in ein Array, um daraus dann die Daten zu bekommen
+        //console.log(formSet);
+        let i: number = 0;
+        while (i < formSet.length) {
+            let form: HTMLElement = formSet[i];
+            form.addEventListener('change', checkValueAndChange); //jedes element in dem Array bekommt nochmals einen Eventlistener, der auf veränderungen des Wert achtet 
+            i++;
+        }
+        document.getElementById('BestellButton').addEventListener('click', completeOrder);//wenn kunde fertig, funktion ausführen, die testet ob alles ausgefüllt
+        document.getElementById('SendeBestellung').addEventListener('click', submitData);
+    }
+
+    function writeHTMLIceFlavor(_currentData: fieldsetData, _currentID: string): void {
+        console.log(_currentData);
+        let optData = document.createElement("option");
+        let htmlString = `
+            <option value="${_currentData.name}">${_currentData.name}</option>
+        
+        `;
+
+        optData.innerHTML = htmlString;
+        document.getElementById(_currentID).appendChild(optData);
+    }
+    function writeHTML(_currentData: fieldsetData, _currentID: string, _labelID: string): void {
+
+        console.log(_currentData);
+        let topData = document.createElement("div");
+        let htmlString = `
+            <input type="${_currentData.type}" value="${_currentData.name}" name="${_currentData.value}" id="${_labelID}"> 
+            <label for="${_labelID}">${_currentData.name}</label>
+        
+        `;
+
+        topData.innerHTML = htmlString;
+        document.getElementById(_currentID).appendChild(topData);
+
+
+    }
+    let iceWorth: number = 0;
+    let iceQuantity1: number = 0;
+    let iceQuantity2: number = 0;
+    let iceQuantity3: number = 0;
+    let iceQuantity4: number = 0;
+    let iceFlavor1: string;
+    let iceFlavor2: string;
+    let iceFlavor3: string;
+    let iceFlavor4: string;
+    let toppingNumber: number = 0;
+    let userName: string;
+    let userStreet: string;
+    let userHousnr: string;
+    let userPLZ: string;
+    let UserCity: string;
+    let userMail: string;
+    let userBecher: string;
+    let userGabel: string;
+    let userShippingSpeed: string;
+    let userAGB: boolean;
+    let userPayment:boolean;
+
+    function checkValueAndChange(_event: Event): void {
+        console.log(_event);
+        let change: HTMLInputElement = <HTMLInputElement>_event.target;
+        //zuerst die Anzahl der Eiskugeln bestimmen 
+        if (change.name == "Stepper1") {
+            if (iceFlavor1 == undefined) { alert("Bitte zuerst die Eissorte Wählen") }
+            else {
+                iceQuantity1 = parseInt(change.value);
+                writeIceChoice(iceQuantity1, iceFlavor1, "Sorte1");
+                renderPrize();
+            }
+        }
+        else if (change.name == "Stepper2") {
+            if (iceFlavor2 == undefined) { alert("Bitte zuerst die Eissorte Wählen") }
+            else {
+                iceQuantity2 = parseInt(change.value);
+                writeIceChoice(iceQuantity2, iceFlavor2, "Sorte2");
+                renderPrize();
+            }
+        }
+        else if (change.name == "Stepper3") {
+            if (iceFlavor3 == undefined) { alert("Bitte zuerst die Eissorte Wählen") }
+            else {
+                iceQuantity3 = parseInt(change.value);
+                writeIceChoice(iceQuantity3, iceFlavor3, "Sorte3");
+                renderPrize();
+            }
+        }
+        else if (change.name == "Stepper4") {
+            if (iceFlavor4 == undefined) { alert("Bitte zuerst die Eissorte Wählen") }
+            else {
+                iceQuantity4 = parseInt(change.value);
+                writeIceChoice(iceQuantity4, iceFlavor4, "Sorte4");
+                renderPrize();
+            }
+        }
+        //Bestimmung welche Eissorte gewählt wurde. 
+        else if (change.name == "Select1") {
+            iceFlavor1 = change.value;
+            writeIceChoice(iceQuantity1, iceFlavor1, "Sorte1");
+        }
+        else if (change.name == "Select2") {
+            iceFlavor2 = change.value;
+            writeIceChoice(iceQuantity2, iceFlavor2, "Sorte2");
+        }
+        else if (change.name == "Select3") {
+            iceFlavor3 = change.value;
+            writeIceChoice(iceQuantity3, iceFlavor3, "Sorte3");
+        }
+        else if (change.name == "Select4") {
+            iceFlavor4 = change.value;
+            writeIceChoice(iceQuantity4, iceFlavor4, "Sorte4");
+        }
+        //In welchem Gefäß möchte der Kunde sein Eis ? 
+        else if (change.value == "Becher") {
+            renderCategories("Becher", "BechoWaff");
+            userBecher = change.value;
+        }
+        else if (change.value == "Waffel") {
+            renderCategories("Waffel", "BechoWaff");
+            userBecher = change.value;
+        }
+
+        else if (change.value == "Schoko-Waffel") {
+            renderCategories("Schoko-Waffel", "BechoWaff");
+            userBecher = change.value;
+        }
+        //jetzt kommen die ganzen Toppings, da toppings einen pauschal Preis haben, wird zubeginn gezählt, wie viele Toppings gewählt wurden
+        else if (change.type == "checkbox") {
+
+            if (change.checked == true) {
+                toppingNumber += 1;
+                let pCon = document.createElement('p');
+                let span = `${change.value}`;
+
+                pCon.innerHTML = span;
+
+                document.getElementById("TopÜbersicht").appendChild(pCon);
+                renderPrize();
+            }
+
+            if (change.checked == false) {
+                toppingNumber -= 1;
+                document.getElementById(change.name).innerHTML = "";
+                renderPrize();
+            }
+        }
+        //jetzt kommt die Abfrage gabel oder löffel
+
+        else if (change.value == "Löffel") {
+            renderCategories("Löffel", "GaboLoff");
+            userGabel = change.value;
+        }
+        else if (change.value == "Gabel") {
+            renderCategories("Gabel", "GaboLoff");
+            userGabel = change.value;
+        }
+        else if (change.value == "Stäbchen") {
+            renderCategories("Stäbchen", "GaboLoff");
+            userGabel = change.value;
+        }
+        else if (change.value == "Zwei Löffel") {
+            renderCategories("Zwei Löffel", "GaboLoff");
+            userGabel = change.value;
+        }
+
+        // jetzt kommt der Part mit den Delivery Arten
+        else if (change.value == "Same Day Delivery") {
+            renderCategories("noch am selben Tag", "ShipTime");
+            userShippingSpeed = change.value;
+        }
+        else if (change.value == "Same Hour Delivery") {
+            renderCategories("noch in der selben Stunde", "ShipTime");
+            userShippingSpeed = change.value;
+        }
+        else if (change.value == "5-10 Werktage ") {
+            renderCategories("in 5-10 Werktagen", "ShipTime");
+            userShippingSpeed = change.value;
+        }
+        else if (change.value == "Beamen") {
+            renderCategories(" das in den nächsten 10 Sekunden ", "ShipTime");
+            userShippingSpeed = change.value;
+        }
+        // hier die Abfragen über Namen und co 
+        else if (change.name == "Name") {
+            renderCategories(change.value, "UserName");
+            userName = change.value;
+        }
+        else if (change.name == "Mail") {
+            renderCategories(change.value, "UserMail")
+            userMail = change.value;
+        }
+        else if (change.name == "Street") {
+            renderCategories(change.value, "UserStreet")
+            userStreet = change.value;
+        }
+        else if (change.name == "Hausnr") {
+            renderCategories(change.value, "UserHouseNum")
+            userHousnr = change.value;
+        }
+        else if (change.name == "PLZ") {
+            renderCategories(change.value, "UserPLZ")
+            userPLZ = change.value;
+        }
+        else if (change.name == "Stadt") {
+            renderCategories(change.value, "UserCity")
+            UserCity = change.value;
+        }
+        else if (change.name == "Telenr") {
+            renderCategories(change.value, "UserTele")
+        }
+        else if (change.name == "Agbgroup") {
+            //console.log("test2")
+            userAGB = true;
+        }
+        else if(change.name == "Zahlungsgroup"){
+            userPayment = true;
+        }
+    }
+
+        function completeOrder(_event: Event): void {
+            if (iceQuantity1 + iceQuantity2 + iceQuantity3 + iceQuantity4 < 1) {
+                alert("Du musst Eis bestellen um Eis zu bekommen...");
+            }
+            else if (userName == undefined) {
+                alert("Ohne Namen wird es sehr schwer das Eis zu liefen.");
+            }
+            else if (userMail == undefined) {
+                alert("Bitte gibt uns noch deine Mail-Adresse an, damit wir die Bestätigung versenden können");
+            }
+            else if (userHousnr == undefined) {
+                alert("Bitte gib noch deine Hausnummer an");
+            }
+            else if (userPLZ == undefined) {
+                alert("Na, wie sieht es aus, welche PLZ hast du?");
+            }
+            else if (userStreet == undefined) {
+                alert("Bitte gib uns noch deine Straße an");
+            }
+            else if (UserCity == undefined) {
+                alert("Bitte gib uns noch die Stadt an, in der du lebst");
+            }
+            else if (toppingNumber == 0) {
+                alert("Möchtest du keine Toppings? ;)");
+            }
+            else if (userBecher == undefined) {
+                alert("In was möchtest du dein Eis?");
+            }
+            else if (userShippingSpeed == undefined) {
+                alert("Wie schnell soll dein Eis dich erreichen?");
+            }
+            else if (userGabel == undefined) {
+                alert("Möchtest du keinen Löffel?");
+            }
+            else if (userAGB == undefined) {
+                console.log("test")
+                alert("Bitte kreuze die AGB optionen an!");
+            }
+            else if(userPayment == undefined) {
+                alert("Bitte gib noch eine Zahlungsmöglichkeit an");
+            }
+            else {
+                alert("Vielen Dank für deine Bestellung, sie wird in kürze bearbeitet")
+            }
+        }
+
+    
+
+
+    function writeIceChoice(_x: number, _y: string, _html: string): void {
+        document.getElementById(_html).innerHTML = "";
+        let pCon = document.createElement('p');
+        let p = `<span>  ${_x} Kugel(n) ${_y}</span>`;
+
+        pCon.innerHTML = p;
+
+        document.getElementById(_html).appendChild(pCon);
+
+    }
+   
+    //funkion die die Darreichungsform und Löffel anzeigt
+    function renderCategories(_r: string, _id: string): void {
+        document.getElementById(_id).innerHTML = "";
+        let pCon = document.createElement('span');
+        let span = `${_r}`;
+        pCon.innerHTML = span;
+        document.getElementById(_id).appendChild(pCon);
+
+        renderPrize();
+    }
+
+
+
+    function renderPrize(): void {
+        iceWorth = (1.55 * (iceQuantity1 + iceQuantity2 + iceQuantity3 + iceQuantity4)) + (toppingNumber * 0.80);
+
+        document.getElementById('Preis').innerHTML = "";
+        let pCon = document.createElement('p');
+        let span = `Zu zahlen: ${iceWorth}€`;
+
+        pCon.innerHTML = span;
+
+        document.getElementById('Preis').appendChild(pCon);
+
+    }
+
+    //funktion mit den Server Sachen:
+
+    function submitData(): void {
+        console.log("Submit gefunden"); 
+        let urlSchreiben: string = "https://ios-eia2.herokuapp.com/";
+        let xhr: XMLHttpRequest = new XMLHttpRequest();
+        for (let i: number = 0; i < iceDealerData["Eissorten"].length; i++) {
+            urlSchreiben += `${iceDealerData["Eissorten"][i].name}&`;
+        }
+
+
+        xhr.open("GET", urlSchreiben, true);
+        xhr.send();
+        console.log(urlSchreiben);
+    }
+
+
+}
+
+
+
