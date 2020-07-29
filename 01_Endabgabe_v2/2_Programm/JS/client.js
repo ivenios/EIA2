4a6d510a0c2cd7608c0224caf1c7ce31ad1e7c2d
Iven Otis Sieglen <iven.otis.sieglen@gmail.com>
2020-07-29 22:35:24 +0200

    First Design steps for HTML

--- 01_Endabgabe_v2/2_Programm/JS/client.js
+++ 01_Endabgabe_v2/2_Programm/JS/client.js
@@ -1,9 +1,9 @@
 var rudeMSPaint;
 (function (rudeMSPaint) {
-    let serverAddress = "";
+    let serverAddress = " x ";
     //zunächst zwei Globale Variablen für den eingeloggten Nutzer und das Bild welches der Nutzer in der akltuelle Sitzung geöffnet hat. 
-    let globalUser = "";
-    let globalPicture = "";
+    let globalUser = "X";
+    let globalPicture = " x";
     document.addEventListener("DOMContentLoaded", init);
     //hier gehts los
     function init() {
--- 01_Endabgabe_v2/2_Programm/JS/client.ts
+++ 01_Endabgabe_v2/2_Programm/JS/client.ts
@@ -1,17 +1,16 @@
 namespace rudeMSPaint {
 
-    let serverAddress: string =""; 
+    let serverAddress: string = " x "; 
     //zunächst zwei Globale Variablen für den eingeloggten Nutzer und das Bild welches der Nutzer in der akltuelle Sitzung geöffnet hat. 
-    let globalUser: string="";
-    let globalPicture: string=""
+    let globalUser: string= "X";
+    let globalPicture: string= " x";
     document.addEventListener("DOMContentLoaded", init);
 
     //hier gehts los
-    function init(): void{
+    function init(): void {
 
         
-    }
-
+     }
 
 
 
