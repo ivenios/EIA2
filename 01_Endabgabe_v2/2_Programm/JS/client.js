2fc27b3cab44f881860bed8bb812e2c100732891
Iven Otis Sieglen <iven.otis.sieglen@gmail.com>
2020-07-31 12:01:27 +0200

    more typo fixing

--- 01_Endabgabe_v2/2_Programm/JS/client.js
+++ 01_Endabgabe_v2/2_Programm/JS/client.js
@@ -13,8 +13,6 @@ var endabgabe2;
     //zunächst zwei Globale Variablen für den eingeloggten Nutzer und das Bild welches der Nutzer in der akltuelle Sitzung geöffnet hat. 
     let globalUser;
     let globalPicture;
-    //let canvas: HTMLCanvasElement = document.querySelector("canvas");
-    //let crc2: CanvasRenderingContext2D = canvas.getContext("2d");
     document.addEventListener("DOMContentLoaded", init);
     //hier gehts los
     function init() {
