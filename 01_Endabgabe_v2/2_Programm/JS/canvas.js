6ba0637b36a3df50a3eec4cb01f7081fae688d63
Iven Otis Sieglen <iven.otis.sieglen@gmail.com>
2020-08-01 21:47:28 +0200

    erstellung des canvas HTMLs

--- 01_Endabgabe_v2/2_Programm/JS/canvas.js
+++ 01_Endabgabe_v2/2_Programm/JS/canvas.js
@@ -10,5 +10,47 @@ var endabgabe2;
     */
     let fps = 30;
     let imgData;
+    let placeableObjectsArray = [];
+    //Trail init funkttion
+    document.addEventListener("DOMContentLoaded", initCanvas);
+    function initCanvas() {
+        // alle EventListener für die Buttons
+        document.getElementById("squareButt").addEventListener("click", initCanvas);
+        document.getElementById("squareButt").addEventListener("click", initCanvas);
+        document.getElementById("squareButt").addEventListener("click", initCanvas);
+        document.getElementById("squareButt").addEventListener("click", initCanvas);
+        document.getElementById("squareButt").addEventListener("click", initCanvas);
+        endabgabe2.canvas = document.getElementsByTagName("canvas")[0];
+        endabgabe2.crc = endabgabe2.canvas.getContext("2d");
+        imgData = endabgabe2.crc.getImageData(0, 0, endabgabe2.canvas.width, endabgabe2.canvas.height);
+        // Hier muss dann die Zahl eingetragen werden, die vom Server gezogen wird:
+        endabgabe2.canvas.width = 1330;
+        endabgabe2.canvas.height = 750;
+        drawBackground();
+    }
+    endabgabe2.initCanvas = initCanvas;
+    function drawBackground() {
+        let eau = new Path2D();
+        eau.rect(0, 0, 1330, 750);
+        endabgabe2.crc.fillStyle = "#92D1FF";
+        endabgabe2.crc.strokeStyle = "#92D1FF";
+        endabgabe2.crc.fill(eau);
+        endabgabe2.crc.stroke(eau);
+        let sol = new Path2D();
+        sol.rect(0, 600, 1330, 150);
+        endabgabe2.crc.fillStyle = "#B3B237";
+        endabgabe2.crc.strokeStyle = "#B3B237";
+        endabgabe2.crc.fill(sol);
+        endabgabe2.crc.stroke(sol);
+    }
+    function updateObject() {
+        window.setTimeout(updateObject, 1000 / fps);
+        endabgabe2.crc.clearRect(0, 0, endabgabe2.canvas.width, endabgabe2.canvas.height);
+        endabgabe2.crc.putImageData(imgData, 0, 0);
+        for (let i = 0; i < placeableObjectsArray.length; i++) {
+            console.log(placeableObjectsArray[i]);
+            placeableObjectsArray[i].updateObject();
+        }
+    }
 })(endabgabe2 || (endabgabe2 = {}));
 //# sourceMappingURL=canvas.js.map
--- 01_Endabgabe_v2/2_Programm/JS/canvas.ts
+++ 01_Endabgabe_v2/2_Programm/JS/canvas.ts
@@ -14,10 +14,59 @@ export let crc: CanvasRenderingContext2D;
 export let canvas: HTMLCanvasElement;
 let fps: number = 30;
 let imgData: ImageData;
+let placeableObjectsArray: PlaceableObjects[] = [];
+
+//Trail init funkttion
+document.addEventListener("DOMContentLoaded", initCanvas);
+
+export function initCanvas(): void {
+    // alle EventListener für die Buttons
+    document.getElementById("squareButt").addEventListener("click", initCanvas);
+    document.getElementById("squareButt").addEventListener("click", initCanvas);
+    document.getElementById("squareButt").addEventListener("click", initCanvas);
+    document.getElementById("squareButt").addEventListener("click", initCanvas);
+    document.getElementById("squareButt").addEventListener("click", initCanvas);
 
 
+    canvas = document.getElementsByTagName("canvas")[0];
+    crc = canvas.getContext("2d");
+    imgData = crc.getImageData(0, 0, canvas.width, canvas.height);
+    // Hier muss dann die Zahl eingetragen werden, die vom Server gezogen wird:
+    canvas.width = 1330;
+    canvas.height = 750;
+
+    drawBackground();
 
 
+}
+
+function drawBackground(): void {
+    let eau: Path2D = new Path2D();
+    eau.rect(0, 0, 1330, 750);
+    crc.fillStyle = "#92D1FF";
+    crc.strokeStyle = "#92D1FF";
+    crc.fill(eau);
+    crc.stroke(eau);
+
+    let sol: Path2D = new Path2D();
+    sol.rect(0, 600, 1330, 150);
+    crc.fillStyle = "#B3B237";
+    crc.strokeStyle = "#B3B237";
+    crc.fill(sol);
+    crc.stroke(sol);
+}
+
+function updateObject(): void {
+    window.setTimeout(updateObject, 1000 / fps);
+    crc.clearRect(0, 0, canvas.width, canvas.height);
+    crc.putImageData(imgData, 0, 0);
+
+    for (let i: number = 0; i < placeableObjectsArray.length; i++) {
+        console.log(placeableObjectsArray[i]);
+        placeableObjectsArray[i].updateObject();
+    }
+
+}
 
 
 }
